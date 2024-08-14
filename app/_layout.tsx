63c34c73153dba98a504160eed9f1ba38f37bda7
sheppardjm <jsheppard@neucadia.com>
2024-08-14 12:42:54 -0400

    Add modal screens

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -4,6 +4,14 @@ export default function Layout() {
   return (
     <Stack>
       <Stack.Screen name="index" options={{ title: "Shopping List" }} />
+      <Stack.Screen
+        name="counter"
+        options={{ title: "Counter", presentation: "modal" }}
+      />
+      <Stack.Screen
+        name="idea"
+        options={{ title: "Idea", presentation: "modal" }}
+      />
     </Stack>
   );
 }
